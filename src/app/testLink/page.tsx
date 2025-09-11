b27f905fc75ab6fc9f32f030faef13ab368c5995
ishimaruryouto <2240006@ecc.ac.jp>
2025-09-11 20:11:03 +0900

    完成

---
+++ src/app/testLink/page.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+export default function TestLink() {
+    return (
+        <>
+            <h1>mannmann</h1>
+            <Link href={"../"}>戻れる</Link>
+        </>
+    )
+}
