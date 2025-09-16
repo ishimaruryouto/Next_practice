dd832fd2936b33489692e548051bc20436680661
ishimaruryouto <2240006@ecc.ac.jp>
2025-09-16 18:46:20 +0900

    問題1完成

--- src/app/useState1/page.tsx
+++ src/app/useState1/page.tsx
@@ -1,9 +1,9 @@
 'use client';
 // ReactのuseStateをインポート
-
+import { useState } from "react";
 export default function Home() {
 	// const [状態変数, 状態を更新する関数] = useState(初期値);
-
+	const [count, setCount] = useState(0);
 	return (
 		<>
 			<div className="flex flex-col items-center justify-center min-h-screen py-2 space-y-4">
@@ -13,14 +13,18 @@ export default function Home() {
 				<div className="flex space-x-2">
 					<button
 						//クリックされるたびに状態変数に1を足す
-
+						onClick={() => setCount(
+							count + 1
+						)}
 						className="bg-blue-500 text-white px-4 py-2 rounded w-20"
 					>
 						+
 					</button>
 					<button
 						//クリックされるたびに状態変数から1を引く
-
+						onClick={() => setCount(
+							count - 1
+						)}
 						className="bg-blue-500 text-white px-4 py-2 rounded w-20"
 					>
 						−
@@ -28,7 +32,7 @@ export default function Home() {
 				</div>
 
 				{/* 状態変数の値を表示する */}
-				<p className="text-lg"></p>
+				<p className="text-lg">{count}</p>
 			</div>
 		</>
 	);
