3d7c3361267934a21d805b31f8b94259e3f25089
ishimaruryouto <2240006@ecc.ac.jp>
2025-09-17 14:29:18 +0900

    レスポンシブ対応完成

--- src/app/practice1/page.tsx
+++ src/app/practice1/page.tsx
@@ -4,7 +4,7 @@ export default function Bike() {
 	return (
 		<>
 			<header>
-				<nav className="flex justify-between text-sm items-center w-3/5 mx-auto py-2.5">
+				<nav className="flex justify-between text-sm items-center w-3/5 mx-auto py-2.5 max-sm:px-3.75 max-sm:w-full">
 					<h1>
 						<Image src="/img/logo.svg" alt="Profile" width={120} height={40} />
 					</h1>
@@ -32,16 +32,16 @@ export default function Bike() {
 					<div className="flex justify-center mt-20">
 						<h2 className="text-[32px] border-b inline">About</h2>
 					</div>
-					<div className="flex w-2/5 mx-auto mt-17.5">
+					<div className="flex w-2/5 mx-auto mt-17.5 max-sm:w-full max-sm:flex-wrap max-sm:px-3.75">
 						<Image
 							src="/img/about.jpg"
 							alt="緑の帽子を被った男性"
 							width={100}
 							height={100}
-							className="rounded-full"
+							className="h-fit rounded-full aspect-square max-sm:block max-sm:mx-auto"
 						/>
 						<div className="ml-7.5">
-							<h3 className="text-base font-bold">KAKERU MIYAICHI</h3>
+							<h3 className="text-base font-bold max-sm:mt-2.5">KAKERU MIYAICHI</h3>
 							<p>
 								テキストテキストテキストテキストテキストテキストテキスト
 								テキストテキストテキストテキストテキストテキストテキスト
@@ -52,24 +52,24 @@ export default function Bike() {
 				</section>
 
 				<section>
-					<div className="flex justify-center mt-20">
+					<div className="flex justify-center mt-20 max-sm:w-full">
 						<h2 className="text-[32px] border-b inline">Bicycle</h2>
 					</div>
-					<div className="flex justify-center gap-4.5 mt-15">
+					<div className="sm:flex justify-center gap-4.5 mt-15 max-sm:flex-wrap max-sm:gap-7.5">
 						<div>
-							<Image src="/img/bicycle1.jpg" alt="黒い自転車のサドル" width={300} height={300} />
+							<Image src="/img/bicycle1.jpg" alt="黒い自転車のサドル" width={300} height={300} className="max-sm:w-full max-sm:px-3.75" />
 							<h3 className="text-base font-bold text-center mt-2.5">タイトルタイトル</h3>
-							<p className="text-sm text-center mt-2.5">みよしみよしみよし</p>
+							<p className="text-sm text-center mt-2.5">テキストテキストテキスト</p>
 						</div>
 
-						<div>
-							<Image src="/img/bicycle2.jpg" alt="シルバーの自転車" width={300} height={300} />
+						<div className="max-sm:mt-7.5">
+							<Image src="/img/bicycle2.jpg" alt="シルバーの自転車" width={300} height={300} className="max-sm:w-full max-sm:px-3.75" />
 							<h3 className="text-base font-bold text-center mt-2.5">タイトルタイトル</h3>
-							<p className="text-sm text-center mt-2.5">ともづかともづかともづか</p>
+							<p className="text-sm text-center mt-2.5">テキストテキストテキスト</p>
 						</div>
 
-						<div>
-							<Image src="/img/bicycle3.jpg" alt="白の自転車" width={300} height={300} />
+						<div className="max-sm:mt-7.5">
+							<Image src="/img/bicycle3.jpg" alt="白の自転車" width={300} height={300} className="max-sm:w-full max-sm:px-3.75" />
 							<h3 className="text-base font-bold text-center mt-2.5">タイトルタイトル</h3>
 							<p className="text-sm text-center mt-2.5">テキストテキストテキスト</p>
 						</div>
